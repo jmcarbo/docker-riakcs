0286acfb605a7a24e457f55601ee6cc0d1869d56
Joan Marc Carbo Arnau <jmcarbo@gmail.com>
2014-01-03 17:34:33 +0100

    Fixed dockerfile

--- conditioning.sh
+++ conditioning.sh
@@ -6,14 +6,15 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/stanchion/app.config
 perl -pi -e 's/{anonymous_user_creation, false}/{anonymous_user_creation, true}/g' /etc/riak-cs/app.config
 
 riak start 
+sleep 4
 stanchion start 
+sleep 4
 riak-cs start
 sleep 5
-echo `curl -sS -H 'Content-Type: application/json' -X POST http://localhost:8080/riak-cs/user --data '{"email":"jmcarbo@gmail.com", "name":"admin user"}'`
-#> /admin_user.json
-#cat /admin_user.json
-#KEY=`cat /admin_user.json | grep -E -o '"key_id":"[^\"]+"' | sed -e 's/\"//g' | cut -d : -f 2`
-#SECRET=`cat /admin_user.json | grep -E -o '"key_secret":"[^\"]+"' | sed -e 's/\"//g' | cut -d : -f 2`
+curl -sS -H 'Content-Type: application/json' -X POST http://localhost:8080/riak-cs/user --data '{"email":"jmcarbo@gmail.com", "name":"admin user"}' > /admin_user.json
+cat /admin_user.json
+KEY=`cat /admin_user.json | grep -E -o '"key_id":"[^\"]+"' | sed -e 's/\"//g' | cut -d : -f 2`
+SECRET=`cat /admin_user.json | grep -E -o '"key_secret":"[^\"]+"' | sed -e 's/\"//g' | cut -d : -f 2`
 
 echo "Admin Key: "$KEY
 echo "Admin Secret: "$SECRET
