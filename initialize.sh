f91746a128b977d56ee64404d8b17d2e7c969e0b
Joan Marc Carbo Arnau <jmcarbo@gmail.com>
2014-01-04 10:28:44 +0100

    Fixed initialize timings

--- initialize.sh
+++ initialize.sh
@@ -2,16 +2,19 @@
 
 rm -rf /var/lib/riak/*
 perl -pi -e 's/{anonymous_user_creation, false}/{anonymous_user_creation, true}/g' /etc/riak-cs/app.config
-mkdir -p /var/lib/riak/ring
 chown -R riak:riak /var/lib/riak        
+echo "Starting nodes"
 riak start 
+sleep 4
 stanchion start 
+sleep 4
 riak-cs start
-sleep 6
-curl -sS -H 'Content-Type: application/json' -X POST http://localhost:8080/riak-cs/user --data '{"email":"jmcarbo@gmail.com", "name":"admin user"}' > /var/lib/riak/admin_user.json
+sleep 5
+echo "Creating admin user"
+curl -sS -H 'Content-Type: application/json' -X POST http://localhost:8080/riak-cs/user --data '{"email":"jmcarbo@gmail.com", "name":"admin user"}' |tee /var/lib/riak/admin_user.json
 KEY=`cat /var/lib/riak/admin_user.json | grep -E -o '"key_id":"[^\"]+"' | sed -e 's/\"//g' | cut -d : -f 2`
 SECRET=`cat /var/lib/riak/admin_user.json | grep -E -o '"key_secret":"[^\"]+"' | sed -e 's/\"//g' | cut -d : -f 2`
-
+sleep 6
 riak-cs stop
 stanchion stop
 riak stop
